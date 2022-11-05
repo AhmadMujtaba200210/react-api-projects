9b84e9156771198787774b9eaa53d44b13294202
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-05 12:26:54 +0500

    feat{App.js}: rendering fix (uncompleted)

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -3,6 +3,14 @@ import React,{useState,useEffect} from "react";
 function App() {
   const [endPoints,setEndPoints]=useState('');  // using state to 
   const [containers,setContainers]=useState([]);
+  const [finalPoints,setFinalPoint]=useState('');
+  
+  useEffect( ()=>{  // will rerender the data when ever we have change in endPoints only
+    fetchMe()
+  },[finalPoints])// rerendering every word when we hit the submit button instead of every letter 
+
+
+  const fetchMe=()=>{
   const options = {
     method: 'GET',
     headers: {
@@ -13,20 +21,37 @@ function App() {
   
   fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoints}`, options)
     .then(response => {
-      return response.json() //returning the JSON response instead of just consoling on log
+      return response.json(); //returning the JSON response instead of just consoling on log
     })
-    .then(data=>{
+    .then(data => {
       setContainers(data.d);// storing the data returned from the server respective to query
     })
     .catch(err => console.error(err));
+  }
+
+    const onChangeHandler = (e) => { // to read the data from input 'e' will be the target every value we entered into the input bar
+      setEndPoints(e.target.value);
+    }
+
+    const submitHandler = e =>{ // for preventing the whole page to reload on every submit 
+      e.preventDefault()
+      setFinalPoint(endPoints)
+    }
   return (
     <div className="App">
 
-    <form >
-      <input type="text" />
+    <form onSubmit={submitHandler}>
+      <input type="text" value={endPoints} onChange={onChangeHandler}/>
       <button type='submit'>Submit</button>
     </form>
-
+      {containers.map((item)=>{
+        return (
+          <div>
+          <img src={item.i.imageUrl} alt="reload the page" />
+          <p>{item.l}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
